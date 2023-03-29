f9d92813588d3dfcfeeb84bbc6d74ee2d83e443b
David Chisnall <davidchisnall@users.noreply.github.com>
2023-03-29 22:02:45 +0300

    Small fixes in timing output. (#43)
    On Sail, use minstret instead of mcycle, since it gives a useful number
    and mcycle does not.

--- tests/test-runner.cc
+++ tests/test-runner.cc
@@ -16,12 +16,9 @@ namespace
 	{
 		int cycles;
 #ifdef SAIL
-		// The Sail model doesn't implement the unprivileged cycle CSR in
-		// M-mode only configurations, so we use mcycle instead. This requires
-		// that mcycle is accessible read-only without the ASR permission.
-		// Probably Sail should have the cycle CSR even without U-mode but need
-		// clarification on spec.
-		__asm__ volatile("csrr %0, mcycle" : "=r"(cycles));
+		// On Sail, report the number of instructions, the cycle count is
+		// meaningless.
+		__asm__ volatile("csrr %0, minstret" : "=r"(cycles));
 #else
 		__asm__ volatile("rdcycle %0" : "=r"(cycles));
 #endif
