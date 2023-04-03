8e193e3416be26795e9d610a7591024029d9753d
Robert Norton <robert.norton@microsoft.com>
2023-04-03 13:57:15 +0100

    Extract get_stack_size function for use in benchmarks.

--- benchmarks/timing.h
+++ benchmarks/timing.h
@@ -1,3 +1,4 @@
+#include <cheri.hh>
 
 namespace
 {
@@ -15,5 +16,20 @@ namespace
 		__asm__ volatile("rdcycle %0" : "=r"(cycles));
 #endif
 		return cycles;
+	}
+
+	/**
+	 * Utility function to return the size of the current stack based on the
+	 * length of csp capability register. If used in a thread entry point this
+	 * will return the full length of the current stack, but if used after a
+	 * compartment call csp is restricted to the part of the stack that is not
+	 * in use by calling compartment(s), so the length will be smaller.
+	 */
+	size_t get_stack_size()
+	{
+		register void *cspRegister asm("csp");
+		asm("" : "=C"(cspRegister));
+		CHERI::Capability stack{cspRegister};
+		return stack.length();
 	}
 } // namespace
