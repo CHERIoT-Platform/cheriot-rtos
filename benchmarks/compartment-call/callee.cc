b4b6204e9b539a79c902245a9df26f1b3e309e8f
Robert Norton <robert.norton@microsoft.com>
2023-07-03 17:10:34 +0100

    Simplify compartment-call benchmark and make it more self consistent.
    Due to the way the call and return costs for this benchmark were computed they didn't
    always add up to the total for the full call. We can eliminate this inconsistency by
    recording the start and end times on either side of the call and having the call return the
    time in the middle and computing the call and return times from this. The result is much
    simpler, sure to be self-consistent and the result for the full call is the same as before
    (at least on Sail) because reading the cycle counter simply takes the place of an
    instruction that otherwise placed zero into a0 for the 'noop' call.

--- benchmarks/compartment-call/callee.cc
+++ benchmarks/compartment-call/callee.cc
@@ -1,10 +1,5 @@
 #include "callee.h"
 #include "../timing.h"
 
-void  noop() {}
-int  noop_return() { return rdcycle(); }
-int  noop_call(int start)
-{
-	return rdcycle() - start;
-}
+int  noop_return_rdcycle() { return rdcycle(); }
 
