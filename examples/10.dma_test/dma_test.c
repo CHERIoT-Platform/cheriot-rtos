ead572bbb99045110e2324c9313458517d1b5a0f
Nazerke Turtayeva <t-nturtayeva@microsoft.com>
2023-07-18 11:45:43 +0100

    fixed some errors and added a test example

--- examples/10.dma_test/dma_test.c
+++ examples/10.dma_test/dma_test.c
@@ -2,9 +2,13 @@
 // SPDX-License-Identifier: MIT
 
 #include <compartment.h>
+#include <cstdint>
+#include <cstdlib>
 #include <debug.hh>
+#include <utils.hh>
+
 #include <fail-simulator-on-error.h>
-#include <platform/ibex/platform-dma.hh>
+#include <../core/dma/dma.h>
 
 /// Expose debugging features unconditionally for this compartment.
 using Debug = ConditionalDebug<true, "Simple DMA request compartment">;
@@ -12,8 +16,13 @@ using Debug = ConditionalDebug<true, "Simple DMA request compartment">;
 /// Thread entry point.
 void __cheri_compartment("dma") dma_request()
 {
-	// Print hello world, along with the compartment's name to the default UART.
 	Debug::log("DMA request app entered!");
 
+	// This is a dma process between two different memory addresses
+	uint32_t *sourceAddress =(uint32_t*) malloc(1024);
+	uint32_t *targetAddress =(uint32_t*) malloc(1024);
 	
+	DMA::Device dmaDevice;
+
+	dmaDevice.configure_and_launch(sourceAddress, targetAddress, 1024, 0, 0, 0);
 }
--- sdk/core/dma/dma.h
+++ sdk/core/dma/dma.h
@@ -1,9 +1,9 @@
 #pragma once
 
-#include "dma_compartment.cc"
 #include "dma_compartment.h"
 #include "platform/ibex/platform-dma.hh"
 #include <concepts>
+#include <cstddef>
 #include <stdint.h>
 #include <utils.hh>
 
@@ -17,34 +17,34 @@ namespace DMA
 	concept IsDmaDevice = requires(T device, uint32_t *sourceAddress, uint32_t *targetAddress, size_t lengthInBytes,
                                         uint32_t sourceStrides, uint32_t targetStrides, uint32_t byteSwapAmount)
 	{
-		{device.init()};
 		{
-			device.write_conf(sourceAddress, destinationAddress, lengthInBytes)
+			device.write_conf(sourceAddress, targetAddress, lengthInBytes)
 			} -> std::same_as<void>;
 		{
 			device.start_dma()
 			} -> std::same_as<void>;
         {
-			device.reset_dma(sourceAddress, destinationAddress)
+			device.reset_dma()
 			} -> std::same_as<void>;
 	};
 
-    template<template<typename, size_t>
-	         typename PlatformDMA>
-	requires IsDmaDevice<PlatformDMA<WordT, TCMBaseAddr>>
-	class GenericDMA : public PlatformDMA<WordT, TCMBaseAddr>
+    template<typename PlatformDMA>
+
+	requires IsDmaDevice<PlatformDMA>
+
+	class GenericDMA : public PlatformDMA
 	{
 		public:
 
-        int configure_and_launch_dma(uint32_t *sourceAddress, uint32_t *targetAddress, uint32_t lengthInBytes,
+        int configure_and_launch(uint32_t *sourceAddress, uint32_t *targetAddress, uint32_t lengthInBytes,
                         uint32_t sourceStrides, uint32_t targetStrides, uint32_t byteSwapAmount)
         {
             return dma_compartment(sourceAddress, targetAddress, lengthInBytes,
-                        sourceStrides, targetStrides, byteSwapAmount)
+                        sourceStrides, targetStrides, byteSwapAmount);
         }
 
 	};
 
-    using DMA = GenericDMA<uint32_t, OtherInformation, PlatformDMA>;
+    using Device = GenericDMA<PlatformDMA>;
 }
 
