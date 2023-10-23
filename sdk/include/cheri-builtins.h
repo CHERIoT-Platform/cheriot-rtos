044dfe793584fce2752243a9fea68272fc9d6c2d
David Chisnall <github@theravensnest.org>
2023-10-23 09:49:10 +0100

    Update microvium
    The newer version in npm has some bytecode format changes and so this is
    needed for people to be able to run the JavaScript examples.
    
    The newer version introduced a snprintf dependency for float -> string
    and int -> string conversion.  We compile in no-float mode, so this is a
    huge dependency for something quite simple.  I have upstreamed a hook to
    allow users to provide a 32-bit integer to float conversion routine and
    added an implementation of it in microvium_port.h.
    
    While poking things upstream, I also added hooks to set bounds and
    permissions on pointers.  These are now used when getting a pointer to a
    string or a buffer from Microvium, which means that the JavaScript heap
    is now fully memory safe when used from C++ code.

--- sdk/include/cheri-builtins.h
+++ sdk/include/cheri-builtins.h
@@ -19,23 +19,21 @@
 
 #define CHERI_OTYPE_BITS 3
 
-#ifdef __cplusplus
-#error Do not use this file from C++.
-#endif
-#ifndef __ASSEMBLER__
+#ifndef __cplusplus
+#	ifndef __ASSEMBLER__
 
-#	include <stddef.h>
-#	include <stdint.h>
+#		include <stddef.h>
+#		include <stdint.h>
 
-#	define cr_read(name)                                                      \
-		({                                                                     \
-			void *val;                                                         \
-			__asm __volatile("cmove %0, " #name : "=C"(val));                  \
-			val;                                                               \
-		})
+#		define cr_read(name)                                                  \
+			({                                                                 \
+				void *val;                                                     \
+				__asm __volatile("cmove %0, " #name : "=C"(val));              \
+				val;                                                           \
+			})
 
-#	define cr_write(name, val)                                                \
-		({ __asm __volatile("cmove " #name ", %0" ::"C"(val)); })
+#		define cr_write(name, val)                                            \
+			({ __asm __volatile("cmove " #name ", %0" ::"C"(val)); })
 
 static inline void mem_cpy64(volatile uint64_t *dst, volatile uint64_t *p)
 {
@@ -45,50 +43,51 @@ static inline void mem_cpy64(volatile uint64_t *dst, volatile uint64_t *p)
 	*_dst = *_p;
 }
 
-#	define cgetlen(foo) __builtin_cheri_length_get(foo)
-#	define cgetperms(foo) __builtin_cheri_perms_get(foo)
-#	define cgettype(foo) __builtin_cheri_type_get(foo)
-#	define cgettag(foo) __builtin_cheri_tag_get(foo)
-#	define cgetoffset(foo) __builtin_cheri_offset_get(foo)
-#	define csetoffset(a, b) __builtin_cheri_offset_set((a), (b))
-#	define cincoffset(a, b) __builtin_cheri_offset_increment((a), (b))
-#	define cgetaddr(a) __builtin_cheri_address_get(a)
-#	define csetaddr(a, b) __builtin_cheri_address_set((a), (b))
-#	define cgetbase(foo) __builtin_cheri_base_get(foo)
-#	define candperms(a, b) __builtin_cheri_perms_and((a), (b))
-#	define cseal(a, b) __builtin_cheri_seal((a), (b))
-#	ifdef FLUTE
-#		define cunseal(a, b)                                                  \
-			({                                                                 \
-				__auto_type __a   = (a);                                       \
-				__auto_type __b   = (b);                                       \
-				__auto_type __ret = __builtin_cheri_tag_clear(__a);            \
-				if (__builtin_cheri_tag_get(__a) &&                            \
-					__builtin_cheri_tag_get(__b) &&                            \
-					__builtin_cheri_type_get(__a) &&                           \
-					!__builtin_cheri_type_get(__b))                            \
-				{                                                              \
-					__auto_type __type = __builtin_cheri_type_get(__a);        \
-					__auto_type __base = __builtin_cheri_base_get(__b);        \
-					if ((__type >= __base) &&                                  \
-						(__type < (__base + __builtin_cheri_length_get(__b)))) \
+#		define cgetlen(foo) __builtin_cheri_length_get(foo)
+#		define cgetperms(foo) __builtin_cheri_perms_get(foo)
+#		define cgettype(foo) __builtin_cheri_type_get(foo)
+#		define cgettag(foo) __builtin_cheri_tag_get(foo)
+#		define cgetoffset(foo) __builtin_cheri_offset_get(foo)
+#		define csetoffset(a, b) __builtin_cheri_offset_set((a), (b))
+#		define cincoffset(a, b) __builtin_cheri_offset_increment((a), (b))
+#		define cgetaddr(a) __builtin_cheri_address_get(a)
+#		define csetaddr(a, b) __builtin_cheri_address_set((a), (b))
+#		define cgetbase(foo) __builtin_cheri_base_get(foo)
+#		define candperms(a, b) __builtin_cheri_perms_and((a), (b))
+#		define cseal(a, b) __builtin_cheri_seal((a), (b))
+#		ifdef FLUTE
+#			define cunseal(a, b)                                              \
+				({                                                             \
+					__auto_type __a   = (a);                                   \
+					__auto_type __b   = (b);                                   \
+					__auto_type __ret = __builtin_cheri_tag_clear(__a);        \
+					if (__builtin_cheri_tag_get(__a) &&                        \
+						__builtin_cheri_tag_get(__b) &&                        \
+						__builtin_cheri_type_get(__a) &&                       \
+						!__builtin_cheri_type_get(__b))                        \
 					{                                                          \
-						__ret = __builtin_cheri_unseal((a), (b));              \
+						__auto_type __type = __builtin_cheri_type_get(__a);    \
+						__auto_type __base = __builtin_cheri_base_get(__b);    \
+						if ((__type >= __base) &&                              \
+							(__type <                                          \
+							 (__base + __builtin_cheri_length_get(__b))))      \
+						{                                                      \
+							__ret = __builtin_cheri_unseal((a), (b));          \
+						}                                                      \
 					}                                                          \
-				}                                                              \
-				__ret;                                                         \
-			})
-#	else
-#		define cunseal(a, b) __builtin_cheri_unseal((a), (b))
-#	endif
-#	define csetbounds(a, b) __builtin_cheri_bounds_set((a), (b))
-#	define csetboundsext(a, b) __builtin_cheri_bounds_set_exact((a), (b))
-#	define ccheckperms(a, b) __builtin_cheri_perms_check((a), (b))
-#	define cchecktype(a, b) __builtin_cheri_type_check((a), (b))
-#	define cbuildcap(a, b) __builtin_cheri_cap_build((a), (b))
-#	define ccopytype(a, b) __builtin_cheri_cap_type_copy((a), (b))
-#	define ccseal(a, b) __builtin_cheri_conditional_seal((a), (b))
-#	define cequalexact(a, b) __builtin_cheri_equal_exact((a), (b))
+					__ret;                                                     \
+				})
+#		else
+#			define cunseal(a, b) __builtin_cheri_unseal((a), (b))
+#		endif
+#		define csetbounds(a, b) __builtin_cheri_bounds_set((a), (b))
+#		define csetboundsext(a, b) __builtin_cheri_bounds_set_exact((a), (b))
+#		define ccheckperms(a, b) __builtin_cheri_perms_check((a), (b))
+#		define cchecktype(a, b) __builtin_cheri_type_check((a), (b))
+#		define cbuildcap(a, b) __builtin_cheri_cap_build((a), (b))
+#		define ccopytype(a, b) __builtin_cheri_cap_type_copy((a), (b))
+#		define ccseal(a, b) __builtin_cheri_conditional_seal((a), (b))
+#		define cequalexact(a, b) __builtin_cheri_equal_exact((a), (b))
 
 static inline size_t ctestsubset(void *a, void *b)
 {
@@ -111,16 +110,18 @@ static inline size_t croundreplen(size_t len)
 	return ret;
 }
 
-#	define cspecial_write(csr, val)                                           \
-		({ __asm __volatile("cspecialw " #csr ", %0" ::"C"(val)); })
+#		define cspecial_write(csr, val)                                       \
+			({ __asm __volatile("cspecialw " #csr ", %0" ::"C"(val)); })
 
-#	define cspecial_read(csr)                                                 \
-		({                                                                     \
-			void *val;                                                         \
-			__asm __volatile("cspecialr %0, " #csr : "=C"(val));               \
-			val;                                                               \
-		})
+#		define cspecial_read(csr)                                             \
+			({                                                                 \
+				void *val;                                                     \
+				__asm __volatile("cspecialr %0, " #csr : "=C"(val));           \
+				val;                                                           \
+			})
 
-#endif // __ASSEMBLER__
+#	endif // __ASSEMBLER__
+
+#endif
 
 #endif // _CHERI_BUILTINS_
