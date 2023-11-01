e15ea6ea892afc330217c88a1e4040a23e56d4ff
David Chisnall <github@theravensnest.org>
2023-11-01 16:25:33 +0000

    If apt is installed, try to use it to install npm.
    This should make the exercise Just Work in the dev container.

--- exercises/01.compartmentalisation/load_js.sh
+++ exercises/01.compartmentalisation/load_js.sh
@@ -14,8 +14,14 @@ fi
 
 if [ ! -f ./node_modules/.bin/microvium ] ; then
 	if ! type npm >/dev/null 2>&1 ; then
-		echo npm is not installed.  Please install it.
-		exit 1
+		if type apt >/dev/null 2>&1 ; then
+			echo npm is not installed.  Trying to install it from apt.
+			echo If you are not using the devcontainer, this may fail.
+			sudo apt install nodejs make gcc g++
+		else
+			echo npm is not installed.  Please install it.
+			exit 1
+		fi
 	fi
 	npm install microvium
 fi
