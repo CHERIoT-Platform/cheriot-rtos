76d73454bcca14180d8aed861704e1c16e097218
David Chisnall <github@theravensnest.org>
2024-01-02 13:36:50 +0000

    Add strlcpy and fix strncpy.
    We have a strcpy implementation that wraps strncpy, so we end up trying
    to pad 4 GiBs of zeroes.  Fix it to wrap strlcpy and add strlcpy.

--- sdk/include/string.h
+++ sdk/include/string.h
@@ -16,8 +16,10 @@ char *__cheri_libcall  strncpy(char *dest, const char *src, size_t n);
 int __cheri_libcall    strcmp(const char *s1, const char *s2);
 char *__cheri_libcall  strstr(const char *haystack, const char *needle);
 char *__cheri_libcall  strchr(const char *s, int c);
+size_t __cheri_libcall strlcpy(char *dest, const char *src, size_t n);
+
 
 __always_inline static inline char *strcpy(char *dst, const char *src)
 {
-	return strncpy(dst, src, SIZE_MAX);
+	return dst + strlcpy(dst, src, SIZE_MAX);
 }
--- sdk/lib/string/xmake.lua
+++ sdk/lib/string/xmake.lua
@@ -1,3 +1,3 @@
 library("string")
   set_default(false)
-  add_files("strcmp.c", "strlen.c", "strncpy.c", "strstr.c", "strchr.c")
+  add_files("strcmp.c", "strlen.c", "strncpy.c", "strstr.c", "strchr.c", "strlcpy.c")
