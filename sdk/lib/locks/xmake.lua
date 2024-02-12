e9bec043aca785b765bcf58e78287500956a9493
Hugo Lefeuvre <hugo.lefeuvre@scisemi.com>
2024-02-12 14:16:21 +0100

    Expose locking subsystem debugging through --debug-locks.
    Signed-off-by: Hugo Lefeuvre <hugo.lefeuvre@scisemi.com>

--- sdk/lib/locks/xmake.lua
+++ sdk/lib/locks/xmake.lua
@@ -1,5 +1,11 @@
 includes("../atomic")
 
+debugOption("locks")
+
 library("locks")
+  add_rules("cheriot.component-debug")
   add_deps("atomic4")
   add_files("locks.cc", "semaphore.cc")
+  on_load(function (target)
+	target:set('cheriot.debug-name', "locks")
+  end)
