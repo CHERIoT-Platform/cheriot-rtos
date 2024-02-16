8648e38ff1d166ffe2ba861ad467c13bdfdd4707
Hugo Lefeuvre <hugo.lefeuvre@scisemi.com>
2024-02-16 20:24:30 +0100

    Enforce bounds in check_pointer `EnforceStrictPermissions` mode.
    The previous commit added a strict permission mode to
    `CHERI::check_pointer` for capability permissions. This commit extends
    this behavior to bounds.
    
    Signed-off-by: Hugo Lefeuvre <hugo.lefeuvre@scisemi.com>

--- tests/check_pointer-test.cc
+++ tests/check_pointer-test.cc
@@ -14,15 +14,18 @@ using namespace CHERI;
  * This test checks the following:
  *
  * 1. When passed `EnforceStrictPermissions = false` (default behavior),
- *    `CHERI::check_pointer` does not modify the permissions of passed
- *    capability with a Capability argument.
+ *    `CHERI::check_pointer` does not modify the permissions and length of
+ *    passed capability with a Capability argument.
  * 2. When passed `EnforceStrictPermissions = false` (default behavior),
- *    `CHERI::check_pointer` does not modify the permissions of passed
- *    capability with a raw capability argument.
+ *    `CHERI::check_pointer` does not modify the permissions and length of
+ *    passed capability with a raw capability argument.
  * 3. When passed `EnforceStrictPermissions = true`, `CHERI::check_pointer`
- *    changes permissions appropriately with a Capability argument.
+ *    changes permissions and length appropriately with a Capability argument.
  * 4. When passed `EnforceStrictPermissions = true`, `CHERI::check_pointer`
- *    changes permissions appropriately with a raw capability argument.
+ *    changes permissions and length appropriately with a raw capability
+ *    argument.
+ * 5. For both 3. and 4., it also checks that `EnforceStrictPermissions = true`
+ *    does not invalidate the capability.
  */
 void check_pointer_strict_mode(int *ptr)
 {
@@ -33,18 +36,27 @@ void check_pointer_strict_mode(int *ptr)
 	debug_log("Test with Capability argument.");
 
 	PermissionSet permissionsAtCreation = cap.permissions();
+	auto          boundsAtCreation      = cap.bounds();
 
 	// Check that the capability has > load permissions at creation.
 	TEST(permissionsAtCreation.contains(Permission::Load, Permission::Store),
 	     "Permissions of new capability do not include load/store.");
 
+	// Check that the capability has length > 1 at creation.
+	TEST(boundsAtCreation > 1,
+	     "Permissions of new capability do not include load/store.");
+
 	// We will detect if `check_pointer` changes the permissions because we
-	// are calling with load only here.
+	// are calling load only and bounds = 1 here.
 	TEST(check_pointer<PermissionSet{Permission::Load}>(cap, 1),
 	     "Cannot check valid capability (calling with Capability object).");
 
 	TEST(cap.permissions() == permissionsAtCreation,
 	     "check_pointer reduced the permissions of passed Capability "
+	     "object although EnforceStrictPermissions = false.");
+
+	TEST(cap.bounds() == boundsAtCreation,
+	     "check_pointer reduced the bounds of passed Capability "
 	     "object although EnforceStrictPermissions = false.");
 
 	debug_log("Test with raw capability argument.");
@@ -56,6 +68,10 @@ void check_pointer_strict_mode(int *ptr)
 
 	TEST(capShouldBeTheSame.permissions() == permissionsAtCreation,
 	     "check_pointer reduced the permissions of passed raw "
+	     "capability although EnforceStrictPermissions = false.");
+
+	TEST(capShouldBeTheSame.bounds() == boundsAtCreation,
+	     "check_pointer reduced the bounds of passed Capability "
 	     "capability although EnforceStrictPermissions = false.");
 
 	debug_log("Test check_pointer with EnforceStrictPermissions = true.");
@@ -72,6 +88,15 @@ void check_pointer_strict_mode(int *ptr)
 	TEST(cap.permissions() == PermissionSet{Permission::Load},
 	     "check_pointer did not reduce the permissions of passed Capability "
 	     "object although EnforceStrictPermissions = true.");
+
+	// The bounds should now be 1.
+	TEST(cap.bounds() == 1,
+	     "check_pointer did not reduce the bounds of passed Capability "
+	     "object although EnforceStrictPermissions = true.");
+
+	TEST(check_pointer<PermissionSet{Permission::Load}>(cap, 1),
+	     "EnforceStrictPermissions = true breaks the capability when called "
+	     "with a Capability object.");
 
 	debug_log("Test with raw capability argument.");
 
@@ -93,6 +118,14 @@ void check_pointer_strict_mode(int *ptr)
 	TEST(cap3.permissions() == PermissionSet{Permission::Load},
 	     "check_pointer did not reduce the permissions of passed raw "
 	     "capability although EnforceStrictPermissions = true.");
+
+	TEST(cap3.bounds() == 1,
+	     "check_pointer did not reduce the bounds of passed raw "
+	     "capability although EnforceStrictPermissions = true.");
+
+	TEST(check_pointer<PermissionSet{Permission::Load}>(ptr, 1),
+	     "EnforceStrictPermissions = true breaks the capability when called "
+	     "with a raw capability.");
 }
 
 void test_check_pointer()
