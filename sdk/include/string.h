d268dc7282a37174d248a81956983361aa0db094
Sam Leffler <sleffler@google.com>
2024-09-03 09:37:03 -0700

    string.h: adjust memchr return value
    C defines the return value without const;
    see https://en.cppreference.com/w/c/string/byte/memchr.
    
    Change-Id: I65faf54d748f2eb2cf6bc9296686006a572592ce

--- sdk/include/string.h
+++ sdk/include/string.h
@@ -10,7 +10,7 @@ int __cheri_libcall   memcmp(const void *str1, const void *str2, size_t count);
 void *__cheri_libcall memcpy(void *dest, const void *src, size_t n);
 void *__cheri_libcall memset(void *, int, size_t);
 void *__cheri_libcall memmove(void *dest, const void *src, size_t n);
-const void *__cheri_libcall memchr(const void *, int, size_t);
+void *__cheri_libcall memchr(const void *, int, size_t);
 size_t __cheri_libcall      strlen(const char *str);
 int __cheri_libcall         strncmp(const char *s1, const char *s2, size_t n);
 char *__cheri_libcall       strncpy(char *dest, const char *src, size_t n);
