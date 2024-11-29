d87f021550d837de0e687c93584dba47d7160454
Nathaniel Wesley Filardo <wes.filardo@scisemi.com>
2024-11-29 14:39:53 +0000

    switcher: symbolize the export entry IRQ flags
    Statically assert that the switcher's possibly imprecise test is precise
    
    This changes no bytes in the generated code.

--- sdk/core/switcher/export-table-assembly.h
+++ sdk/core/switcher/export-table-assembly.h
@@ -11,3 +11,7 @@ EXPORT_ASSEMBLY_OFFSET(ExportTable, pcc, 0)
 EXPORT_ASSEMBLY_OFFSET(ExportTable, cgp, 8)
 EXPORT_ASSEMBLY_OFFSET(ExportTable, errorHandler, 16)
 EXPORT_ASSEMBLY_OFFSET(ExportTable, errorHandlerStackless, 18)
+
+EXPORT_ASSEMBLY_EXPRESSION(ExportEntryInterruptStatusSwitcherMask,
+                           ExportEntry::InterruptStatusSwitcherMask,
+                           0x10)
