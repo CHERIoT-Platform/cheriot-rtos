233d52441123e00efdf20a43d5d1e48288720b23
Owen Anderson <resistor@mac.com>
2025-01-13 21:25:58 +1300

    Further fixes for enabling warnings on cross-compartment void returns.

--- examples/05.sealing/caller.cc
+++ examples/05.sealing/caller.cc
@@ -8,11 +8,13 @@
 using Debug = ConditionalDebug<true, "Caller compartment">;
 
 /// Thread entry point.
-void __cheri_compartment("caller") entry()
+int __cheri_compartment("caller") entry()
 {
 	auto *identifier = identifier_create(42);
 	Debug::log("Allocated identifier to hold the value 42: {}", identifier);
 	Debug::log("Value is {}", identifier_value(identifier));
-	identifier_destroy(identifier);
+	Debug::Invariant(identifier_destroy(identifier) >= 0,
+	                 "Unable to call identifier_destroy");
 	Debug::log("Dangling pointer: {}", identifier);
+	return 0;
 }
