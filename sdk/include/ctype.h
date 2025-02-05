a11be1efaeeb4e2d6767f8077db08232568bc761
Nathaniel Wesley Filardo <wes.filardo@scisemi.com>
2025-02-05 14:51:20 +0000

    Complete ctype.h

--- sdk/include/ctype.h
+++ sdk/include/ctype.h
@@ -3,14 +3,24 @@
 
 #pragma once
 
-static inline int isprint(int c)
+static inline int isascii(int c)
 {
-	return c >= '\x20' && c <= '\x7e';
+	return (c & ~0x7F) == 0;
 }
 
 static inline int isdigit(int c)
 {
 	return c >= '0' && c <= '9';
+}
+
+static inline int islower(int c)
+{
+	return (c >= 'a') && (c <= 'z');
+}
+
+static inline int isprint(int c)
+{
+	return c >= '\x20' && c <= '\x7e';
 }
 
 static inline int isspace(int c)
@@ -29,3 +39,28 @@ static inline int isspace(int c)
 	}
 }
 
+static inline int isupper(int c)
+{
+	return (c >= 'A') && (c <= 'Z');
+}
+
+static inline int isalpha(int c)
+{
+	return islower(c) || isupper(c);
+}
+
+static inline int isxdigit(int c)
+{
+	return isdigit(c) || ((c >= 'A') && (c <= 'F')) ||
+	       ((c >= 'a') && (c <= 'f'));
+}
+
+static inline int tolower(int c)
+{
+	return c + (isupper(c) ? 0x20 : 0);
+}
+
+static inline int toupper(int c)
+{
+	return c - (islower(c) ? 0x20 : 0);
+}
