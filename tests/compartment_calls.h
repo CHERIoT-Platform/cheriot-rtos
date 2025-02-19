44c1bdeba1278354ec379e36d87fa33c6dc10b0c
Owen Anderson <resistor@mac.com>
2025-02-19 20:58:40 +1300

    clang-format with clang-18

--- tests/compartment_calls.h
+++ tests/compartment_calls.h
@@ -40,7 +40,7 @@ __cheri_compartment("compartment_calls_inner") int compartment_call_inner(
   int        x6);
 __cheri_compartment("compartment_calls_inner") int test_incorrect_export_table(
   __cheri_callback void (*fn)(),
-  bool                 *outTestFailed);
+  bool *outTestFailed);
 __cheri_compartment(
   "compartment_calls_inner_with_"
   "handler") int test_incorrect_export_table_with_handler(__cheri_callback int (*fn)());
