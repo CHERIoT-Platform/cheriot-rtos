eded80b2375cdc1242dd011dfa6ddc89db8ba2c6
David Chisnall <github@theravensnest.org>
2025-03-07 11:18:55 +0000

    Reduce scheduler permissions slightly.
    The scheduler doesn't require store permission for futexes when it does
    futex_wake, so don't require any permissions here.  This means that you
    can spuriously wake any thread waiting on a futex that you have any
    pointer to, even a read-only one, but the API permits spurious wakes.
    
    The benefit from this is that the scheduler cannot tamper with a futex
    word.  For example, if a futex is a trivial lock where 1 indicates locked
    and 0 indicates unlocked, the scheduler currently can both wake waiters
    and set the lock value to 0 making two threads believe that they
    acquired the same lock.  With this change, an attacker who gets
    arbitrary code execution in the scheduler cannot do this, they can only
    cause threads to wake, see that the lock is already acquired, and then
    go back to sleep.
    
    The C++ atomics (used for the locks library) now enforce this least
    privilege when calling the scheduler.
    
    These changes increase the size of the test suite by 32 bytes.

--- sdk/include/futex.h
+++ sdk/include/futex.h
@@ -67,10 +67,14 @@ __always_inline static int futex_wait(const uint32_t *address,
  * Wakes up to `count` threads that are sleeping with `futex[_timed]_wait` on
  * `address`.
  *
- * The `address` argument must permit storing four bytes of data after the
- * address. This call does not store to the address but requiring store
- * permission prevents a thread from waking up a futex that it cannot possibly
- * have moved to a different state.
+ * The `address` argument must be a valid, unsealed, pointer with a length of at
+ * least four after the address but the scheduler does not require any explicit
+ * permissions.  The scheduler never needs store access to the futex word.
+ * Removing store permission means that a compromised scheduler can cause
+ * spurious wakes but cannot tamper with the futex word.  If, for example, the
+ * futex word is a lock then the scheduler can wake threads that are blocked on
+ * the lock but cannot release the lock and so cannot make two threads believe
+ * that they have simultaneously acquired the same lock.
  *
  * The return value for a successful call is the number of threads that were
  * woken.  `-EINVAL` is returned for invalid arguments.
