9f9c6c8c360b16af9d9955edde9092f4debab3e9
Nathaniel Wesley Filardo <wes.filardo@scisemi.com>
2025-03-26 12:28:02 +0000

    Expose hardware revoker concepts from allocator

--- sdk/core/allocator/revoker.h
+++ sdk/core/allocator/revoker.h
@@ -9,6 +9,8 @@
 #include <stdint.h>
 #include <utils.hh>
 
+#include <platform/concepts/hardware_revoker.hh>
+
 #if __has_include(<platform-hardware_revoker.hh>)
 #	include <platform-hardware_revoker.hh>
 #elif defined(TEMPORAL_SAFETY) && !defined(SOFTWARE_REVOKER)
@@ -17,36 +19,6 @@
 
 namespace Revocation
 {
-	/**
-	 * Concept for a hardware revoker.  Boards can provide their own definition
-	 * of this, which must be found in `<hardware_revoker.hh>` in a path
-	 * provided by the board search.
-	 */
-	template<typename T>
-	concept IsHardwareRevokerDevice = requires(T v, uint32_t epoch) {
-		{ v.init() };
-		{ v.system_epoch_get() } -> std::same_as<uint32_t>;
-		{
-			v.template has_revocation_finished_for_epoch<true>(epoch)
-		} -> std::same_as<uint32_t>;
-		{
-			v.template has_revocation_finished_for_epoch<false>(epoch)
-		} -> std::same_as<uint32_t>;
-		{ v.system_bg_revoker_kick() } -> std::same_as<void>;
-	};
-
-	/**
-	 * If this revoker supports an interrupt to notify of completion then it
-	 * must have a method that blocks waiting for the interrupt to fire.  This
-	 * method should return true if the epoch has been reached or false if the
-	 * timeout expired.
-	 */
-	template<typename T>
-	concept SupportsInterruptNotification =
-	  requires(T v, Timeout *timeout, uint32_t epoch) {
-		  { v.wait_for_completion(timeout, epoch) } -> std::same_as<bool>;
-	  };
-
 	/**
 	 * Class for interacting with the shadow bitmap.  This bitmap controls the
 	 * behaviour of a hardware load barrier, which will invalidate capabilities
