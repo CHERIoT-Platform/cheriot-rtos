30531de31f5b7ef4c8c91a951bc4dd58c407d01a
Nathaniel Wesley Filardo <wes.filardo@scisemi.com>
2025-03-27 03:25:09 +0000

    benchmarks/compartment-call: 5 iters; other types
    Run everything 5 times so that we can see cache warming effects.
    For reference, show in-compartment and library calls.
    
    Like interrupt-latency-ext, allow xmake to specify the METRIC under
    study.

--- benchmarks/compartment-call/callee.cc
+++ benchmarks/compartment-call/callee.cc
@@ -1,5 +1,13 @@
 #include "callee.h"
 #include "../timing.h"
 
-int  noop_return_rdcycle() { return rdcycle(); }
+int noop_return_metric()
+{
+	return METRIC();
+}
 
+int stack_using_return_metric(size_t stackUse)
+{
+	use_stack(stackUse);
+	return METRIC();
+}
--- benchmarks/compartment-call/caller.cc
+++ benchmarks/compartment-call/caller.cc
@@ -8,12 +8,17 @@
 
 using Debug = ConditionalDebug<DEBUG_CALLER, "Compartment call benchmark">;
 
+__noinline int local_noop_return_metric()
+{
+	return METRIC();
+}
+
 int __cheri_compartment("caller") run()
 {
-	static std::array<std::tuple<size_t, int, int, int>, 5> results;
-	static int                                              nextResult = 0;
-	static TicketLock                                       lock;
-	static bool                                             headerWritten;
+	static std::array<std::tuple<size_t, int, int, int>, 25> results;
+	static int                                               nextResult = 0;
+	static TicketLock                                        lock;
+	static bool                                              headerWritten;
 	size_t stackSize = get_stack_size();
 	// Make sure that we hit the lock last if we have the biggest stack.  We
 	// don't have enough stack space for the log call on the smallest stacks.
@@ -24,15 +29,21 @@ int __cheri_compartment("caller") run()
 		                 "Compartment call thread_sleep failed");
 	}
 	LockGuard g{lock};
-	auto [full, callPath, returnPath] = CHERI::with_interrupts_disabled([&]() {
-		auto start  = rdcycle();
-		auto middle = noop_return_rdcycle();
-		auto end    = rdcycle();
-		return std::tuple{end - start, middle - start, end - middle};
-	});
-	results[nextResult++]             = {stackSize, full, callPath, returnPath};
+	for (int i = 0; i < 5; i++)
+	{
+		auto [full, callPath, returnPath] =
+		  CHERI::with_interrupts_disabled([&]() {
+			  auto start  = METRIC();
+			  auto middle = noop_return_metric();
+			  auto end    = METRIC();
+			  return std::tuple{end - start, middle - start, end - middle};
+		  });
+		results[nextResult++] = {stackSize, full, callPath, returnPath};
+	}
 	if (stackSize == 0x1000)
 	{
+		printf("---- Results using METRIC: " __XSTRING(METRIC) "\n");
+
 		printf("#board\tstack size\tfull call\tcall\treturn\n");
 		for (auto [stackSize, full, callPath, returnPath] : results)
 		{
@@ -41,6 +52,55 @@ int __cheri_compartment("caller") run()
 			       full,
 			       callPath,
 			       returnPath);
+		}
+
+		printf("#board\tcaller stack\tcallee stack\tfull call\tcall\treturn\n");
+		for (int callerUse = 128; callerUse <= 1024; callerUse <<= 1)
+		{
+			for (int calleeUse = 128; calleeUse <= 1024; calleeUse <<= 1)
+			{
+				for (int i = 0; i < 5; i++)
+				{
+					use_stack(callerUse);
+
+					auto start  = METRIC();
+					auto middle = stack_using_return_metric(calleeUse);
+					auto end    = METRIC();
+
+					printf(__XSTRING(BOARD) "\t%d\t%d\t%d\t%d\t%d\n",
+					       callerUse,
+					       calleeUse,
+					       end - start,
+					       middle - start,
+					       end - middle);
+				}
+			}
+		}
+
+		printf("#function call\n");
+		for (int i = 0; i < 5; i++)
+		{
+			auto start  = METRIC();
+			auto middle = local_noop_return_metric();
+			auto end    = METRIC();
+
+			printf(__XSTRING(BOARD) "\t%d\t%d\t%d\n",
+			       end - start,
+			       middle - start,
+			       end - middle);
+		}
+
+		printf("#library call\n");
+		for (int i = 0; i < 5; i++)
+		{
+			auto start  = METRIC();
+			auto middle = lib_noop_return_metric();
+			auto end    = METRIC();
+
+			printf(__XSTRING(BOARD) "\t%d\t%d\t%d\n",
+			       end - start,
+			       middle - start,
+			       end - middle);
 		}
 	}
 
