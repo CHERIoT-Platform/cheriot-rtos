5153516d25539c241d95731275ffe8d2a22ff011
Robert Norton-Wright <robert.norton@scisemi.com>
2025-07-13 17:53:56 +0100

    Solution to part 2 of compartmentalisation exercise.
    Put JS vm in a compartment so that if it fails it doesn't take down the whole program.

---
+++ exercises/01.compartmentalisation/js.hh
@@ -0,0 +1,1 @@
+int __cheri_compartment("js") run_js (uint8_t *bytecode, size_t sz);
--- exercises/01.compartmentalisation/xmake.lua
+++ exercises/01.compartmentalisation/xmake.lua
@@ -16,27 +16,31 @@ includes(path.join(sdkdir, "lib/freestanding"),
 option("board")
     set_default("ibex-safe-simulator")
 
-compartment("secret")
-    add_files("secret.cc")
+compartment("entry")
+    add_files("entry.cc")
 
 compartment("js")
     add_files("js.cc")
+
+compartment("secret")
+    add_files("secret.cc")
 
 -- Firmware image for the example.
 firmware("javascript")
     add_deps("crt", "freestanding", "string", "microvium", "atomic_fixed")
     add_deps("js")
+    add_deps("entry")
     add_deps("secret")
     add_deps("debug")
     on_load(function(target)
         target:values_set("board", "$(board)")
         target:values_set("threads", {
             {
-                compartment = "js",
+                compartment = "entry",
                 priority = 1,
                 entry_point = "run",
                 stack_size = 0x800,
-                trusted_stack_frames = 4
+                trusted_stack_frames = 5
             }
         }, {expand = false})
     end)
