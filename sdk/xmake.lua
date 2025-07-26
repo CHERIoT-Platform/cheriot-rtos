0b7e107947cb513f5212a9125a4ca3a126285e90
Owen Anderson <resistor@mac.com>
2025-07-26 23:20:12 +0800

    Update a small number of places to use the most current target triple.
    These have been supported for ~6 months, so it seems safe to adopt them at this point.

--- sdk/xmake.lua
+++ sdk/xmake.lua
@@ -136,7 +136,7 @@ toolchain("cheriot-clang")
 		-- Flags used for C/C++ and assembly
 		local default_flags = {
 			"-target",
-			"riscv32cheriot-unknown-unknown",
+			"riscv32cheriot-unknown-cheriotrtos",
 			"-mcpu=cheriot",
 			"-mabi=cheriot",
 			"-mxcheri-rvc",
