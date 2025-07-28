5b142a483ff14e1e54668a9554adc4348f0b1cef
Nathaniel Wesley Filardo <wes.filardo@scisemi.com>
2025-07-28 17:45:08 +0000

    switcher: Check stack perms before spilling
    Full credit to @stellamplau; I just push buttons when told which
    buttons to push.
    
    @stellamplau and @vmurali point out that spilling caller registers
    before doing permission checks means that those registers' contents
    could end up shared memory.  While the caller could always do this to
    itself, defense in depth suggests that we not let the switcher
    exacerbate the situation if the caller's csp has somehow been corrupted.
    Conveniently, in this case, it is exactly zero more instructions to
    perform the permission and alignment checks before the spill.  While we
    could spilt those, I don't think it hurts to do both earlier, since
    we're going to do both eventually anyway.
    
    Tweak the test for untrusted-stack exhaustion during switcher spill so
    that it passes the now earlier alignment check.
    
    Add a test explicitly for the motivating case, by copying cgp over csp
    before the call, which should induce a force-unwind (and does, looking
    at the Sail simulator's trace).
    
    FIXES https://github.com/CHERIoT-Platform/cheriot-rtos/issues/560

--- tests/test-runner.cc
+++ tests/test-runner.cc
@@ -196,7 +196,7 @@ int __cheri_compartment("test_runner") run_tests()
 		run_timed("Compartment calls", test_compartment_call);
 		run_timed("check_pointer", test_check_pointer);
 		run_timed("Misc APIs", test_misc);
-		run_timed("Stacks exhaustion in the switcher", test_stack);
+		run_timed("Switcher stack handling", test_stack);
 		run_timed("Thread pool", test_thread_pool);
 		run_timed("Global Constructors", test_global_constructors);
 		run_timed("Queue", test_queue);
