01c00c002ad91fe4d268580558034c5b149b12f3
Hugo Lefeuvre <hugo.lefeuvre@ubc.ca>
2025-07-29 10:21:20 -0700

    core APIs benchmark: tweaks and fixes.
    Two actual fixes:
    - `heap_claim` and `heap_free` should be called on a heap allocation on
      a different quota, otherwise they do not go through the normal (more
      expensive) call path. The benchmark is calling them on a stack
      variable, making it look twice cheaper than what it really is.
    - Similar problem with `heap_claim_ephemeral`, the difference is about
      40-50 cycles. We need to run it on a heap allocation too.
    
    A few tweaks to match what we want to do in the paper:
    - We want to measure the cost of capability manipulations,
      `check_pointer`, `heap_claim_ephemeral`, and `heap_claim` and
      `heap_free` only. We do not want to couple these measurements with a
      function call or compartment call (otherwise it is annoying, we need
      to do the substraction in the scripts). Run all of these in inlined.
      This does take some care to ensure the compiler doesn't optimize
      things away.
    
    Note that none of these issues affect the paper's measurements, these
    happened during the merge of the patch we used for the paper's numbers.
    
    Signed-off-by: Hugo Lefeuvre <hugo.lefeuvre@ubc.ca>

--- benchmarks/compartment-call/callee.cc
+++ benchmarks/compartment-call/callee.cc
@@ -15,34 +15,6 @@ int callee_stack_using_return_metric(size_t stackUse)
 	return METRIC();
 }
 
-int callee_check_pointer_return_metric(void *p)
-{
-	using namespace CHERI;
-
-	if (!check_pointer<PermissionSet{Permission::Load}>(p))
-	{
-		return -EINVAL;
-	}
-
-	return METRIC();
-}
-
-int callee_ephemeral_claim_return_metric(void *p)
-{
-	Timeout t{UnlimitedTimeout};
-	heap_claim_ephemeral(&t, p);
-
-	return METRIC();
-}
-
-int callee_claim_release_return_metric(void *p)
-{
-	heap_claim(MALLOC_CAPABILITY, p);
-	heap_free(MALLOC_CAPABILITY, p);
-
-	return METRIC();
-}
-
 int callee_dereference(int *p)
 {
 	return *p;
