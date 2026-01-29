4551ca56c5bac85237fcaf85f390e2780dc619c5
David Chisnall <github@theravensnest.org>
2026-01-29 14:53:07 +0000

    Remove the cross-thread interrupt mechanism.
    We eventually want *something like* this, but not this.
    
    It exists in the git history if we want to resurrect it later, but
    currently it's adding TCB complexity for a thing no one is using.  We
    should design something better *with some actual use case implemented*
    and then implement that thing.
    
    Fixes #372

--- sdk/include/switcher.h
+++ sdk/include/switcher.h
@@ -22,30 +22,6 @@ __cheri_libcall _Bool trusted_stack_has_space(int requiredFrames);
 __cheri_libcall void *switcher_recover_stack(void);
 
 /**
- * Returns a sealed capability to the current thread.  This can be used as the
- * argument to `switcher_interrupt_thread` from another thread to interrupt
- * this thread (invoking its error handler) if and only if the other thread is
- * currently executing in this compartment.  This can be used to implement
- * watchdog events from another thread.
- */
-__cheri_libcall void *switcher_current_thread(void);
-
-/**
- * Interrupt given thread (invoke its error handler). This can be used with the
- * value returned by `switcher_current thread` in another thread to interrupt
- * that thread if and only if the other thread is currently executing in the
- * same compartment as this thread. This can be used to implement watchdog
- * events from another thread. An attempt to interrupt the current thread will
- * return failure.
- *
- * Returns true on success, false on failure.
- *
- * This should typically be followed by a yielding operation.  The interrupted
- * thread is not woken until the scheduler runs.
- */
-__cheri_libcall _Bool switcher_interrupt_thread(void *);
-
-/**
  * Returns a store-only capability to two hazard pointer slots for the current
  * thread.  Objects stored here will not be deallocated until (at least) the
  * next cross-compartment call or until they are explicitly overwritten.
@@ -79,4 +55,3 @@ __cheri_libcall ptraddr_t stack_lowest_used_address(void);
  * an error handler is currently running, the remaining bits are the count.
  */
 __cheri_libcall uint16_t switcher_handler_invocation_count_reset(void);
-
